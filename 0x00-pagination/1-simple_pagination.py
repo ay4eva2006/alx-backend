47d5f3d5a8a961e0895614cc5c68a38dfb43c0f4
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-27 12:14:58 +0100

    Pagination task 1 reduce blank lines

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,7 +5,6 @@ import math
 from typing import List
 
 
-
 def index_range(page: int, page_size: int) -> tuple:
     """returns index range for items"""
     return (((page - 1) * page_size), page * page_size)
